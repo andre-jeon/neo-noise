c363a82f4498d3c39732def679f2b78d56ad98e5
Namhyuk Woo <namhyukwoo@Namhyuks-Air.lan>
2021-05-06 14:47:58 -0700

    most functions done, flexbox fixed

--- index.js
+++ index.js
@@ -3,25 +3,73 @@ function togglePopup() {
 }
 
 //Toggling the audio to play and pause
-const playButton = document.getElementById("playbutton");
-const audioFile = document.getElementById("audioFile");
-let state = false;
 
-function toggleAudio() {
-  if (state == false) {
-    audioFile.play();
-    state = true;
+//AUDIO 1
+const audioFile1 = document.getElementById("audioFile1");
+let state1 = false;
+
+function toggleAudio1() {
+  if (state1 == false) {
+    audioFile1.play();
+    state1 = true;
+    console.log("It is now playing audio 1");
   } else {
-    audioFile.pause();
-    state = false;
+    audioFile1.pause();
+    state1 = false;
+    console.log("It has now paused on audio 1");
   }
 }
 //Volume
-const volume = document.getElementById("volume");
-volume.addEventListener("click", (event) => {
-  audioFile.volume = volume.value;
-  console.log(`volume changed to ${volume.value}`);
+const volume1 = document.getElementById("volume1");
+volume1.addEventListener("click", (event) => {
+  audioFile1.volume = volume1.value;
+  console.log(`volume changed to ${volume1.value}`);
 });
+
+//AUDIO 2
+const audioFile2 = document.getElementById("audioFile2");
+let state2 = false;
+
+function toggleAudio2() {
+  if (state2 == false) {
+    audioFile2.play();
+    state2 = true;
+    console.log("It is now playing audio 2");
+  } else {
+    audioFile2.pause();
+    state2 = false;
+    console.log("It has now paused on audio 2");
+  }
+}
+//Volume
+const volume2 = document.getElementById("volume2");
+volume2.addEventListener("click", (event) => {
+  audioFile2.volume = volume2.value;
+  console.log(`volume changed to ${volume2.value}`);
+});
+
+//AUDIO 3
+const audioFile3 = document.getElementById("audioFile3");
+let state3 = false;
+
+function toggleAudio3() {
+  if (state3 == false) {
+    audioFile3.play();
+    state3 = true;
+    console.log("It is now playing audio 3");
+  } else {
+    audioFile3.pause();
+    state3 = false;
+    console.log("It has now paused on audio 3");
+  }
+}
+//Volume
+const volume3 = document.getElementById("volume3");
+volume3.addEventListener("click", (event) => {
+  audioFile3.volume = volume3.value;
+  console.log(`volume changed to ${volume3.value}`);
+});
+
 //Mute toggle
 // function mute() {
 //   if (audioFile.volume > 0) {
